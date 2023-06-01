5845971d2bbb948b548273b2ea6de755fab58359
catsAndIvy <bronwyn-elizabeth@live.co.uk>
2023-06-01 10:25:03 +0100

    add CSS changes

--- src/Components/ReviewCard.jsx
+++ src/Components/ReviewCard.jsx
@@ -7,13 +7,13 @@ function ReviewCard ({review_id, gameTitle, username, image, createdAt, votes, d
         <li key={review_id} className="ReviewListItem" >
             <h3 id="RCTitle">{gameTitle}</h3>
             <img id="RCImage" src={image} alt={gameTitle}/>
-            <p id="RCUsername">  Review added by: {username} </p>
-            <p id="RCCreatedAt"> Submitted on: {createdAt} </p>
-            <p id="RCVotes"> Votes: {votes} </p>
-            <p id="RCDesigner"> Game designer: {designer} </p>
-            <p id="RCCategory"> Category: {category} </p>
-            <p id="RCCommentCount"> Comments: {commentCount} </p>
-            <Link to={`/reviews/${review_id}`} > Click for more information about this game review.</Link>
+            <p id="RCUsername">  <span className="bold">Review added by: </span>{username} </p>
+            <p id="RCCreatedAt"> <span className="bold">Submitted on: </span>{createdAt} </p>
+            <p id="RCVotes"> <span className="bold">Votes: </span>{votes} </p>
+            <p id="RCDesigner"> <span className="bold">Game designer: </span>{designer} </p>
+            <p id="RCCategory"> <span className="bold">Category: </span>{category} </p>
+            <p id="RCCommentCount"> <span className="bold">Comments: </span>{commentCount} </p>
+            <Link to={`/reviews/${review_id}`} className="links"> Click for more information about this game review.</Link>
         </li>
     
     );
