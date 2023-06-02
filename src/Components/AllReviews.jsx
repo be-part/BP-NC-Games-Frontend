d98ffa135c33c694347e0cc01e532180de88dbc4
catsAndIvy <bronwyn-elizabeth@live.co.uk>
2023-06-02 17:03:28 +0100

    add functionality to sort reviews feature

--- src/Components/AllReviews.jsx
+++ src/Components/AllReviews.jsx
@@ -1,4 +1,4 @@
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useLocation, useNavigate  } from "react-router-dom";
 import ReviewCard from "./ReviewCard";
 import {useState, useEffect} from "react"
 import { getReviews } from "../Api";
@@ -6,24 +6,65 @@ import { getReviews } from "../Api";
 function AllReviews (){
 
     const [searchParams, setSearchParams] = useSearchParams();
-    const Category = searchParams.get('category')
+    const reviewCategory = searchParams.get('category')
+
+    const [searchQuery, setSearchQuery] = useState({category: reviewCategory, order: 'DESC', sort_by: 'created_at'});
 
     const [reviewsList, setReviewsList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
+    let { state } = useLocation();
+    
+    // if (state.value === null) {setSearchQuery((currQuery) => {
+    //     const newQuery = {...currQuery}
+    //     newQuery.category=null
+    //     return newQuery
+    // })
+
+
+    // let navigate = useNavigate(); 
+    // const routeChange = () =>{ 
+    //   let path = `${location.pathname}`; 
+    //   navigate(path);
+    // }
+    
+    
 
     useEffect(() => {
-        getReviews(Category).then(({reviews}) => {
+        console.log("HELLO")
+        getReviews(searchQuery).then(({reviews}) => {
             setReviewsList(reviews);
             setIsLoading(false);
         })
-    }, [Category]);
+    }, [searchQuery, state]);
     
     if (isLoading) return <p className="Loading"> Loading... </p>
-    
+
+    const handleOrder = (value) => {
+        setSearchQuery((currQuery)=> {
+            return {...currQuery, order: value}
+        });
+    };
+
+    const handleSortBy = (value) => {
+        setSearchQuery((currQuery)=> {
+            return {...currQuery, sort_by: value}
+        });
+    };
+
     return (<>
     
-    {!Category ? <h2 id="ReviewsListTitle">All Board Game Reviews</h2> : <h2 id="ReviewsListTitle"> All <span id="CategoryTypeTitle">{Category}</span> Board Game Reviews</h2>}
+    {!reviewCategory ? <h2 id="ReviewsListTitle">All Board Game Reviews</h2> : <h2 id="ReviewsListTitle"> All <span id="CategoryTypeTitle">{reviewCategory}</span> Board Game Reviews</h2>}
+
+    <label htmlFor="sortOptions">Sort By: </label>
+    <select name="sortOptions" id="sortOptionsMenu" onChange={(event) => handleSortBy(event.target.value)} >
+        <option value="created_at">Date</option>
+        <option value="comment_count">Comment Count</option>
+        <option value="votes">Votes</option>
+    </select>
+
+    <button onClick={() => handleOrder('ASC')}>Ascending</button>
+    <button onClick={() => handleOrder('DESC')}>Descending</button>
 
     <ul className ="ReviewsList">
     
